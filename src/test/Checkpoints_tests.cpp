aa5811c24306cad32a5f20ab27abb633ad880eaa
WagerrTor <tor@i2pmail.org>
2018-02-17 09:02:55 +0100

    no comment
    Former-commit-id: 2a80762d0c7c68f601ec663a16aa89874ab77927 [formerly 43ac36e9920dbfdaa35a1dcdcf4569dc0b7fe5f4]
    Former-commit-id: b4dc9c28a4299cd1d826d7dd76e2c6fef0db2a14
    Former-commit-id: b4e344f5e19fc04a842f548b518f81c0fad53b3c

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -19,28 +19,23 @@ BOOST_AUTO_TEST_SUITE(Checkpoints_tests)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
-    // WagerrDevs - RELEASE CHANGE - if required, sanity checks
+    // WagerrDevs - RELEASE CHANGE - if required, sanity checks, ot required after pow
     uint256 p0 = uint256("0x000007b9191bc7a17bfb6cedf96a8dacebb5730b498361bf26d44a9f9dcc1079");     // Genesis block
-    /*
-    uint256 p1499 = uint256("0x96bbf9ef28aca64e940444bc54e61d69fbb22c6dfbff445f3230ee613f5204aa");  // Last block
+    uint256 p1001 = uint256("0x0000002a314058a8f61293e18ddbef5664a2097ac0178005f593444549dd5b8c");  // Last PoW block
 
     BOOST_CHECK(Checkpoints::CheckBlock(0, p0));
-    BOOST_CHECK(Checkpoints::CheckBlock(1499, p1499));
+    BOOST_CHECK(Checkpoints::CheckBlock(1001, p1001));
 
     // Wrong hashes at checkpoints should fail:
-    BOOST_CHECK(!Checkpoints::CheckBlock(0, p1499));
-    BOOST_CHECK(!Checkpoints::CheckBlock(1499, p0));
+    BOOST_CHECK(!Checkpoints::CheckBlock(0, p1001));
+    BOOST_CHECK(!Checkpoints::CheckBlock(1001, p0));
 
     // ... but any hash not at a checkpoint should succeed:
-    BOOST_CHECK(Checkpoints::CheckBlock(0+3, p1499));
-    BOOST_CHECK(Checkpoints::CheckBlock(1499+3, p0));
+    BOOST_CHECK(Checkpoints::CheckBlock(0+1, p1001));
+    BOOST_CHECK(Checkpoints::CheckBlock(1001+1, p0));
 
     // remove this later
-    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 1499);
-    */
-
-    BOOST_CHECK(Checkpoints::CheckBlock(0, p0));
-    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 0);
+    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 1001);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
